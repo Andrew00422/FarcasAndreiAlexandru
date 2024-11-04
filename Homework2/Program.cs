c760ca45a321abd0babd1dc53c459d53ce13af12
Andrew00422 <andreialexandru.farcas@ulbsibiu.ro>
2024-11-04 23:47:20 +0200

    .

---
+++ Homework2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace Library
+
+{
+ class Program
+ {
+    static void Main(string[]args)
+    {
+
+        Book book1 = new Book ("White Nights","Fyodor Dostoviesky");
+        book1.CheckOut();
+        Book book2 = new Book ("Luceafarul","Mihai Eminescu",720);
+        book2.ReturnBook();
+
+        Console.WriteLine(book1.ToString());
+        Console.WriteLine(book2.ToString());
+    }
+
+}
+}
